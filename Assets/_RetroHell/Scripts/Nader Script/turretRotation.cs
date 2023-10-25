bd805ebb436b8e99867a10aae37a09bbd1718b5d
Nader Khaled <n.thabit@student.saedubai.com>
2023-10-25 22:15:51 +0400

    simple bullet pattern(turret
    turret rotate on Y axis and fires bullets from 4 directions

---
+++ Assets/_RetroHell/Scripts/Nader Script/turretRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretRotation : MonoBehaviour
+{
+    public float rotationDelay = 1f;
+    private bool rotating = false;
+    void Update()
+    {
+        if (Time.time >= rotationDelay && !rotating)
+        {
+            RotateTurret();
+        }
+    }
+    public void RotateTurret()
+    {
+        transform.Rotate(0, 1, 0);
+    }
+}
