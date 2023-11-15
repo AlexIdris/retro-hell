4d25da738347931175cf48e0fb04733952b0897c
Muyiwa Alex Idris <everyth1ngxconnected@gmail.com>
2023-11-15 05:37:06 +0400

    Slow Motion Text and Edit I

--- Assets/_RetroHell/Scripts/Nader Script/slowMotion.cs
+++ Assets/_RetroHell/Scripts/Nader Script/slowMotion.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,32 +8,54 @@ public class slowMotion : MonoBehaviour
     float slowMoSpeed = 0.5f;
     float normSpeed = 1.0f;
     [SerializeField] Button button;
+    [SerializeField] GameObject SlowmoFrame;
+    [SerializeField] TMP_Text EText;
+    [SerializeField] float delayTime = 2f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         button.interactable = true;
+        SlowmoFrame.SetActive(true);
+        EText.gameObject.SetActive(true);
+
     }
 
     // Update is called once per frame
     public void Update()
     {
+        Slowdowntime();
+        Normaltime();
+
+    }
+    void Slowdowntime()
+    {
+
+
         if (Input.GetKeyDown(KeyCode.E))
         {
+
             slowMo = true;
             Time.timeScale = slowMoSpeed;
             button.onClick.Invoke();
-
+            SlowmoFrame.SetActive(true);
+            EText.gameObject.SetActive(true);
 
         }
-        else if (Input.GetKeyUp(KeyCode.E))
+    }
+    void Normaltime()
+    {
+
+        if (Input.GetKeyUp(KeyCode.E))
         {
             slowMo = false;
             Time.timeScale = normSpeed;
             button.interactable = false;
-        }
-        return;
+            SlowmoFrame.SetActive(false);
+            EText.gameObject.SetActive(false);
 
+
+        }
     }
 }
