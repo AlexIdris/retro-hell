f38d624d9c181d262f04c6342b848037df804de1
Mohammad Thani <mohammad.almheiri@student.saedubai.com>
2023-11-15 20:48:14 +0400

    Pause Menu

---
+++ Assets/_RetroHell/Scripts/Thani Script/PauseSystem.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseSystem : MonoBehaviour
+{
+    public bool gamePaused;
+    public GameObject pauseScreen;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                pauseScreen.SetActive(true);
+                Debug.Log("Game Paused!");
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
+        Debug.Log("Game Resumed!");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Move Test");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
+
