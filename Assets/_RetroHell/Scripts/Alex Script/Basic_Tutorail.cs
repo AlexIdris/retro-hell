b5c57326f657851ea26b47dd393a9fcc3a143139
Muyiwa Alex Idris <everyth1ngxconnected@gmail.com>
2023-11-17 21:32:06 +0400

    Bugs Repair

--- Assets/_RetroHell/Scripts/Alex Script/Basic_Tutorail.cs
+++ Assets/_RetroHell/Scripts/Alex Script/Basic_Tutorail.cs
@@ -6,49 +6,75 @@ public class Basic_Tutorail : MonoBehaviour
     [SerializeField] TMP_Text TutorText;
     [SerializeField] TMP_Text IntructionText;
     [SerializeField] GameObject Frame;
-    [SerializeField] float timeAwake;
     [SerializeField] GameObject Next;
     [SerializeField] GameObject Previous;
     private void Start()
     {
-
-        Frame.SetActive(true);
-        TutorText.gameObject.SetActive(true);
-
-        TutorText.text = "WSAD for Movement\n" +
-            "Left Click Mouse for Shooting\n" +
-            "E to Slowdown Time\n";
-
-        IntructionText.text = "Press 2 for Next";
-        Next.SetActive(true);
-        Previous.SetActive(false);
     }
-    private void Update()
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.tag == "Player")
+        {
 
-        if (Input.GetKey(KeyCode.Alpha2))
-        {
-            TutorText.text = "Blue Box Indicate Machineguns\n " +
-            "Right Click Mouse to Active Machinegun\n" +
-             "Green Box = +50 Health\n" +
-             "Destroy the Enemy\n" +
-             "Try not to Die!";
-            IntructionText.text = "Press 1 to go Back ";
-            Next.SetActive(false);
-            Previous.SetActive(true);
-        }
-        if (Input.GetKey(KeyCode.Alpha1))
-        {
-            TutorText.text = "WSAD for Movement\n" +
-           "Left Click Mouse for Shooting\n" +
-           "E to Slowdown Time\n";
+            Frame.SetActive(true);
+            TutorText.gameObject.SetActive(true);
+
+            TutorText.text = "WASD for Movement\n" +
+                "Left Click Mouse for Shooting\n" +
+                "E to Slowdown Time\n";
+
             IntructionText.text = "Press 2 for Next";
             Next.SetActive(true);
             Previous.SetActive(false);
         }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
 
+            Frame.SetActive(true);
+            TutorText.gameObject.SetActive(true);
 
+            TutorText.text = "WASD for Movement\n" +
+                "Left Click Mouse for Shooting\n" +
+                "E to Slowdown Time\n";
 
+            IntructionText.text = "Press 2 for Next";
+            Next.SetActive(true);
+            Previous.SetActive(false);
+
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                TutorText.text = "Blue Box Indicate Machineguns\n " +
+                "Right Click Mouse to Active Machinegun\n" +
+                 "Green Box = +50 Health\n" +
+                 "Destroy the Enemy\n" +
+                 "Try not to Die!";
+                IntructionText.text = "Press 1 to go Back ";
+                Next.SetActive(false);
+                Previous.SetActive(true);
+            }
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                TutorText.text = "WSAD for Movement\n" +
+               "Left Click Mouse for Shooting\n" +
+               "E to Slowdown Time\n";
+                IntructionText.text = "Press 2 for Next";
+                Next.SetActive(true);
+                Previous.SetActive(false);
+            }
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+        Frame.SetActive(false);
+        TutorText.gameObject.SetActive(false);
+        Next.SetActive(false);
+        Previous.SetActive(false);
 
     }
+
 }
