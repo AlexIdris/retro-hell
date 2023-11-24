b82a6654a024004850e4f5950db5ba149537f8c6
Muyiwa Alex Idris <everyth1ngxconnected@gmail.com>
2023-11-24 13:39:58 +0400

    Monster Asset swap

--- Assets/_RetroHell/Scripts/Nader Script/pingpongTurret.cs
+++ Assets/_RetroHell/Scripts/Nader Script/pingpongTurret.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class pingpongTurret : MonoBehaviour
 {
-    [SerializeField] float minDistance = 5;
-    [SerializeField] float maxDistance = 10;
+    [SerializeField] float minDistance = 2;
+    [SerializeField] float maxDistance = 2;
     [SerializeField] float speed = 3;
 
     void Update()
     {
-        float yAxis = Mathf.PingPong(Time.time * speed, maxDistance - minDistance);
+        float yAxis = Mathf.PingPong(Time.time * speed, maxDistance + minDistance);
         transform.position = new Vector3(transform.position.x, yAxis, transform.position.z);
     }
 }
