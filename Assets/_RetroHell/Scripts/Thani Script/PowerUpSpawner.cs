01c01df850f2428b73259ed2dbfc41a4492cc873
Mohammad Thani <mohammad.almheiri@student.saedubai.com>
2023-12-14 19:07:32 +0400

    Procedural Power-Up Generation?

--- Assets/_RetroHell/Scripts/Thani Script/PowerUpSpawner.cs
+++ Assets/_RetroHell/Scripts/Thani Script/PowerUpSpawner.cs
@@ -6,7 +6,7 @@ public class PowerUpSpawner : MonoBehaviour
     public List<GameObject> PowerUps;
 
     public float timer;
-    public int randomPowerUp;
+    public int PowerUpList = 0;
     public int randomPosition;
 
     public void FixedUpdate()
@@ -15,36 +15,43 @@ public class PowerUpSpawner : MonoBehaviour
 
         if (timer > 5)
         {
-            randomPowerUp = Random.Range(0, 2);
+            if (PowerUpList == 4)
+            {
+                PowerUpList = 0;
+            }
+
             randomPosition = Random.Range(0, 4);
 
             if (randomPosition == 0)
             {
                 Vector3 position = new Vector3(Random.Range(28, 30), -1, Random.Range(-30, 30));
-                Instantiate(PowerUps[randomPowerUp], position, Quaternion.identity);
+                Instantiate(PowerUps[PowerUpList], position, Quaternion.identity);
                 timer = 0;
             }
 
             if (randomPosition == 1)
             {
                 Vector3 position = new Vector3(Random.Range(-30, 30), -1, Random.Range(28, 30));
-                Instantiate(PowerUps[randomPowerUp], position, Quaternion.identity);
+                Instantiate(PowerUps[PowerUpList], position, Quaternion.identity);
                 timer = 0;
             }
 
             if (randomPosition == 2)
             {
                 Vector3 position = new Vector3(Random.Range(-28, -30), -1, Random.Range(-30, 30));
-                Instantiate(PowerUps[randomPowerUp], position, Quaternion.identity);
+                Instantiate(PowerUps[PowerUpList], position, Quaternion.identity);
                 timer = 0;
             }
 
             if (randomPosition == 3)
             {
                 Vector3 position = new Vector3(Random.Range(-30, 30), -1, Random.Range(-28, -30));
-                Instantiate(PowerUps[randomPowerUp], position, Quaternion.identity);
+                Instantiate(PowerUps[PowerUpList], position, Quaternion.identity);
                 timer = 0;
             }
+
+            PowerUpList++;
+            Debug.Log(PowerUpList);
         }
     }
 }
