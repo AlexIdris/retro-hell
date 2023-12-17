a4f1e84a43690f90825c8cb61cd706b157cbf86e
Mohammad Thani <mohammad.almheiri@student.saedubai.com>
2023-12-17 22:53:59 +0400

    Camera now shakes (WIP)

--- Assets/_RetroHell/Scripts/Thani Script/ScreenShake.cs
+++ Assets/_RetroHell/Scripts/Thani Script/ScreenShake.cs
@@ -4,25 +4,46 @@ using UnityEngine;
 
 public class ScreenShake : MonoBehaviour
 {
-    public float timeLimit = 1f;
-
-    public AnimationCurve shakingSpeed;
-
-    public IEnumerator Shake(float timeLimit, float magnitude)
+    public IEnumerator Shake(float timeLimit)
     {
-        Vector3 startPosition = transform.localPosition;
-        float timePassed = 0.0f;
+        float timePassed = 0f;
+        float magnitude = 1f;
+
         while (timePassed < timeLimit)
         {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
+            float x = (Random.value - 0.5f) * magnitude;
+            float y = (Random.value - 0.5f) * magnitude;
 
-            transform.localPosition = new Vector3(x, y, startPosition.z);
+            transform.localPosition = new Vector3(x, y, 0);
 
-            timePassed = Time.deltaTime;
+            timePassed += Time.deltaTime;
+            magnitude = (1 - (timePassed / timeLimit) * 1 - (timePassed / timeLimit));
+
             yield return null;
         }
 
-        transform.position = startPosition;
+        transform.localPosition = Vector3.zero;
     }
+
+    //public float timeLimit = 1f;
+
+    //public AnimationCurve shakingSpeed;
+
+    //public IEnumerator Shake(float timeLimit, float magnitude)
+    //{
+    //Vector3 startPosition = transform.localPosition;
+    //float timePassed = 0.0f;
+    //while (timePassed < timeLimit)
+    //{
+    //float x = Random.Range(-1f, 1f) * magnitude;
+    //float y = Random.Range(-1f, 1f) * magnitude;
+
+    //transform.localPosition = new Vector3(x, y, startPosition.z);
+
+    //timePassed = Time.deltaTime;
+    //yield return null;
+    //}
+
+    //transform.position = startPosition;
+    //}
 }
